50833d21d88c11b71da41b1932cfea64b19b3dd6
Jeremiah Carlson <87202597+jeremiah-carlson@users.noreply.github.com>
2023-08-18 17:33:46 -0400

    Add basic ssl cert conf

---
+++ ssl/localhost/key_gen.sh
@@ -0,0 +1,10 @@
+read -p 'Chose configuration file: ' cert_conf
+read -p 'Chose keyout file name: ' cert_key
+read -p 'Chose csr file name: ' cert_csr
+read -p 'Chose cert file name: ' cert_out
+
+openssl req -new -config $cert_conf -keyout $cert_key -out $cert_csr
+
+openssl x509 -req -in $cert_csr -signkey $cert_key -out $cert_out -days 366 -sha256 -extfile $cert_conf -extensions req_ext
+ 
+
